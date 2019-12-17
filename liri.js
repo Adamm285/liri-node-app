0a233e5fa8ac619aab0161101cbbad19a1ff262c
Adam m <adam@floorsmtihllc.com>
2019-12-16 19:33:49 -0500

    added bonus

--- liri.js
+++ liri.js
@@ -9,6 +9,14 @@ var fs = require('fs')
 var input = process.argv
 var returned = input[2]
 var userInput = input.slice(3).join(" ")
+var lineBreak = "======================="
+// 
+function log_entry(data) {
+    fs.appendFile('log.txt', data + lineBreak, function (err) {
+        if (err) throw err
+        console.log('saved')
+    })
+}
 // 
 function findSpotify() {
     spotify.search({
@@ -18,37 +26,34 @@ function findSpotify() {
         if (err) {
             return console.log('err:' + err)
         }
-        for (let i = 0; i < data.tracks.items.length; i++) {
-            console.log("Artist: " + data.tracks.items[i].artists[0].name)
-            console.log("Song Name: " + data.tracks.items[i].name)
-            console.log("Song Preview: " + data.tracks.items[i].preview_url)
-            console.log("Album Title: " + data.tracks.items[i].album.name)
-        }
+        let addy = '\n' + 'Artist: ' + data.tracks.items[0].artists[0].name + '\n' + "Song Name: " +
+            data.tracks.items[0].name + '\n' + "Song Preview: " + data.tracks.items[0].preview_ur +
+            '\n' + "Album Title: " + data.tracks.items[0].album.name + '\n';
+        console.log(addy)
+        log_entry(addy)
     })
 }
 // 
 function concertThis() {
     axios.get('https://rest.bandsintown.com/artists/' + userInput + '/events?app_id=codingbootcamp')
         .then(function (response) {
-            for (let i = 0; i < response.data.length; i++) {
-                console.log('Venue: ' + response.data[i].venue.name)
-                console.log('Location: ' + response.data[i].venue.city, response.data[0].venue.region)
-                console.log('Date: ' + moment(response.data[i].datetime, "YYYY-MM-DD HH:mm").format("MM/DD/YYYY hh:mm a"))
-            }
+            let addy2 = '\n' + 'Venue: ' + response.data[0].venue.name + '\n' + 'Location: ' + response.data[0].venue.city +
+                response.data[0].venue.region + '\n' + 'Date: ' +
+                moment(response.data[0].datetime, "YYYY-MM-DD HH:mm").format("MM/DD/YYYY hh:mm a") + '\n';
+            console.log(addy2)
+            log_entry(addy2)
+
         })
 }
 // 
 function movieThis() {
-    axios.get('http://www.omdbapi.com/?t=' + userInput + '&apikey=3eb254bb')
+    axios.get("http://www.omdbapi.com/?t=" + userInput + "&apikey=trilogy")
         .then(function (response) {
-            console.log('Title: ' + response.data.Title)
-            console.log('Year: ' + response.data.Year)
-            console.log('Rating 1: ' + response.data.imdbRating)
-            console.log('Rating 2: ' + response.data.Ratings[1].Value)
-            console.log('Film Location: ' + response.data.Country)
-            console.log('Lang.: ' + response.data.Language)
-            console.log('Plot: ' + response.data.Plot)
-            console.log('Actors: ' + response.data.Actors)
+            let addy3 ='\n' +  'Title: ' + response.data.Title + '\n' + 'Year: ' + response.data.Year + '\n' + 'Rating 1: ' + response.data.imdbRating + 
+            '\n' + 'Rating 2: ' + response.data.Ratings[1].Value + '\n' + 'Film Location: ' + response.data.Country + 
+            '\n' + 'Lang.: ' + response.data.Language + '\n' + 'Plot: ' + response.data.Plot + '\n' + 'Actors: ' + response.data.Actors + '\n';    
+            console.log(addy3)
+            log_entry(addy3)
         })
 }
 // 
@@ -64,24 +69,24 @@ function doWhatItSays() {
     })
 }
 // 
-function check(){
-switch (returned) {
-    case "spotify-this-song":
-        findSpotify()
-        break
-    case "concert-this":
-        concertThis()
-        break
-    case "movie-this":
-        movieThis()
-        break
-    case "do-what-it-says":
-        doWhatItSays()
-        break
-    default:
-        console.log("err")
-    break
-}
+function check() {
+    switch (returned) {
+        case "spotify-this-song":
+            findSpotify()
+            break
+        case "concert-this":
+            concertThis()
+            break
+        case "movie-this":
+            movieThis()
+            break
+        case "do-what-it-says":
+            doWhatItSays()
+            break
+        default:
+            console.log("err")
+            break
+    }
 }
 check()
 // 
